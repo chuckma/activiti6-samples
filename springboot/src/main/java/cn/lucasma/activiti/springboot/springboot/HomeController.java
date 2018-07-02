818520fe958c113fff8b9c998daa5024c25987c1
lucas ma <robbincen@163.com>
2018-07-02 21:00:50 +0800

    integrate springboot

---
+++ springboot/src/main/java/cn/lucasma/activiti/springboot/springboot/HomeController.java
@@ -0,0 +1,17 @@
+package cn.lucasma.activiti.springboot.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Lucas Ma
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "Hello World!";
+    }
+}
---
+++ springboot/src/main/java/cn/lucasma/activiti/springboot/springboot/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package cn.lucasma.activiti.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
